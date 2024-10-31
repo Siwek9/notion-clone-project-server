3d422a75b7b96ec66414bddcacfab4b9a612830c
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2024-10-31 10:19:44 +0100

    Add first post
    😒😒

--- app.ts
+++ app.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
-app.get("/  ");
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/check-autorization", (req, res) => {
+  console.log(req.body);
+  res.send(JSON.stringify({ siema: "lol" }));
+});
+
+app.get("/", (req, res) => {
+  res.send("loool");
+});
 
 app.listen(8000, () => {
   console.log("Server started");
