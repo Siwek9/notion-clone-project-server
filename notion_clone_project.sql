6f1a2072a859a778ea04bfdd305d12a020af9928
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2024-11-04 23:19:09 +0100

    Update database

--- notion_clone_project.sql
+++ notion_clone_project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Paź 29, 2024 at 11:07 AM
--- Wersja serwera: 10.4.28-MariaDB
--- Wersja PHP: 8.2.4
+-- Generation Time: Lis 04, 2024 at 11:18 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,6 +49,14 @@ CREATE TABLE `users` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `profile_picture`, `description`) VALUES
+(1, 'Admin', 'admin@admin.pl', 'bdd2297f93550f01452cbd838c276f0dd22f498b4661394f1528ab88d6e63e6f', '', ''),
+(2, 'Siwek9', 'siwek9@siwek9.com', 'bdd2297f93550f01452cbd838c276f0dd22f498b4661394f1528ab88d6e63e6f', '', '');
+
 -- --------------------------------------------------------
 
 --
@@ -58,7 +66,22 @@ CREATE TABLE `users` (
 CREATE TABLE `user_note_relations` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `id_note` int(11) NOT NULL
+  `id_note` int(11) NOT NULL,
+  `owner` tinyint(1) NOT NULL,
+  `can_modify` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user_session`
+--
+
+CREATE TABLE `user_session` (
+  `session_id` varchar(255) NOT NULL,
+  `ip_address` varchar(15) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `expiration_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -84,6 +107,12 @@ ALTER TABLE `user_note_relations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `user_session`
+--
+ALTER TABLE `user_session`
+  ADD PRIMARY KEY (`session_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -97,7 +126,7 @@ ALTER TABLE `notes`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user_note_relations`
